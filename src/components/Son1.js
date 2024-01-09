d732f0bd81412007896d4a3067bd8a8b3b694a68
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-01-09 17:40:57 +0200

    callback

--- src/components/Son1.js
+++ src/components/Son1.js
@@ -1,12 +1,28 @@
+import { useState, useEffect, useCallback } from "react";
 import useName from "../hooks/userName";
 
 const Son1 = (props) => {
     const [name, handleName] = useName(props.name);
+    const [num, setNum] = useState(0);
+
+    const numUp = useCallback(() => {
+        setNum((oldValue) => oldValue + 1);
+    }, [setNum]);
+
+    useEffect(() => {
+        numUp();
+        return () => {
+            setNum(0);
+        }
+    }, [numUp]);
+
 
     return (
         <div>
             <h1>{name}</h1>
             <button onClick={() => handleName('Zohar')}>click</button>
+            <h1>{num}</h1>
+            <button onClick={numUp}>+</button>
         </div>
     );
 }
